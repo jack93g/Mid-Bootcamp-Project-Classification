9ac5fdbc16fe946c652ee084cb4ffc6f3446ac2d
jack93g <jack93g@gmail.com>
2021-08-22 13:30:00 +0200

    adding sql file

---
+++ files/classification.sql
@@ -0,0 +1,9 @@
+create database credit_card_classification;
+
+create table credit_card_data (
+Customer_Number int,
+Offer_Accepted bool,
+Reward varchar(200),
+Mailer_Type varchar(200),
+Income_Level varchar(200),
+
